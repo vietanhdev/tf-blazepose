01d058e817b7be8681330e20eb86ecfa265da96e
Viet Anh Nguyen <vietanh.dev@gmail.com>
2020-12-05 23:15:35 +0700

    Reduce augmentation

--- src/data/augmentation.py
+++ src/data/augmentation.py
@@ -19,11 +19,11 @@ def load_aug():
                 pad_cval=(0, 255)
             )),
             sometimes(iaa.Affine(
-                scale={"x": (0.8, 1.2), "y": (0.8, 1.2)}, # scale images to 80-120% of their size, individually per axis
-                translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)}, # translate by -20 to +20 percent (per axis)
-                rotate=(-20, 20), # rotate by -45 to +45 degrees
-                shear=(-10, 10), # shear by -16 to +16 degrees
-                order=[0, 1], # use nearest neighbour or bilinear interpolation (fast)
+                scale={"x": (0.9, 1.1), "y": (0.9, 1.1)},
+                translate_percent={"x": (-0.05, 0.05), "y": (-0.05, 0.05)},
+                rotate=(-20, 20),
+                shear=(-10, 10),
+                order=[0, 1],
                 cval=(0, 255), # if mode is constant, use a cval between 0 and 255
                 mode=ia.ALL # use any of scikit-image's warping modes (see 2nd image from the top for examples)
             )),
@@ -31,11 +31,10 @@ def load_aug():
             # don't execute all of them, as that would often be way too strong
             iaa.SomeOf((0, 5),
                 [
-                    sometimes(iaa.Superpixels(p_replace=(0, 1.0), n_segments=(20, 200))), # convert images into their superpixel representation
                     iaa.OneOf([
                         iaa.GaussianBlur((0, 3.0)), # blur images with a sigma between 0 and 3.0
-                        iaa.AverageBlur(k=(2, 7)), # blur image using local means with kernel sizes between 2 and 7
-                        iaa.MedianBlur(k=(3, 11)), # blur image using local medians with kernel sizes between 2 and 7
+                        iaa.AverageBlur(k=(2, 5)), # blur image using local means with kernel sizes between 2 and 7
+                        iaa.MedianBlur(k=(3, 5)), # blur image using local medians with kernel sizes between 2 and 7
                     ]),
                     iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)), # sharpen images
                     iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5), # add gaussian noise to images
@@ -53,8 +52,6 @@ def load_aug():
                     ]),
                     iaa.LinearContrast((0.5, 2.0), per_channel=0.5), # improve or worsen the contrast
                     iaa.Grayscale(alpha=(0.0, 1.0)),
-                    sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.05))), # sometimes move parts of the image around
-                    sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.1)))
                 ],
                 random_order=True
             )
